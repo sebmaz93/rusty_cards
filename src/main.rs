6c05dcccacee01426572da4d8debea4fa502ec79
sebmaz <seb.maz93@icloud.com>
2024-08-08 03:49:33 +0400

    Add rng

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
+use rand::{seq::SliceRandom, thread_rng};
+
 #[derive(Debug)]
 struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    let deck: Deck = Deck { cards: vec![
-        "two of hearts",
-        ""
-    ] };
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        let values = [
+            "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack",
+            "Queen", "King",
+        ];
+        let mut cards = vec![];
 
-    println!("Here is your deck: {:?}", deck);
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
+    }
+
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {}
+}
+
+fn main() {
+    let mut deck = Deck::new();
+
+    deck.shuffle();
+
+    println!("Here is your deck: {:#?}", deck);
 }
