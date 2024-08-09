d44390113f38a2e4a92c51e2c39b8abe8933c51b
sebmaz <seb.maz93@icloud.com>
2024-08-09 15:55:44 +0400

    Add deck shuffle and deal

--- src/main.rs
+++ src/main.rs
@@ -29,13 +29,18 @@ impl Deck {
         self.cards.shuffle(&mut rng);
     }
 
-    fn deal(&mut self, num_cards: usize) -> Vec<String> {}
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
 }
 
 fn main() {
     let mut deck = Deck::new();
 
     deck.shuffle();
+    // add error handling for case of more cards than available
+    let cards = deck.deal(3);
 
     println!("Here is your deck: {:#?}", deck);
+    println!("Here are your cards: {:#?}", cards);
 }
